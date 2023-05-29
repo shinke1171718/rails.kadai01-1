8094ca4b77e7c7aa3891313d2399b67d6f3f91e8
shinke <sinke.niinomi0506@icloud.com>
2023-05-29 10:13:02 +0900

    ああああ

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Myapp011
+module Myapp15
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
@@ -18,5 +18,6 @@ module Myapp011
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Tokyo'
   end
 end
